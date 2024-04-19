42abab91e071921f65e35b1818b4c44968f17836
vitor06092006 <108538840+vitor06092006@users.noreply.github.com>
2024-04-19 11:26:53 -0300

    vv

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for(let i=0;i < botoes.length;i++){
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2024-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2025-02-01T00:00:00");
+const tempoObjetivo1 = new Date("2024-05-05T00:00:00");
+const tempoObjetivo2 = new Date("2024-07-05T00:00:00");
+const tempoObjetivo3 = new Date("2024-11-30T00:00:00");
+const tempoObjetivo4 = new Date("2025-08-01T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
